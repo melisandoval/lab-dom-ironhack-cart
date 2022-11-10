3db5a523474a58232f3447532297bf54c958cea7
melisand <melisandoval@live.com.ar>
2022-11-10 13:17:19 +0100

    add iteration 2

--- js/index.js
+++ js/index.js
@@ -1,10 +1,14 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  const price = Number(document.querySelector('.price span').innerHTML);
-  const quantity = Number(document.querySelector('.quantity input').value);
-  let subTotal = document.querySelector('.subtotal span');
+  let price = Number(product.querySelector(`.price span`).innerHTML);
+  // console.log(`price is ${price}`);
 
+  let quantity = Number(product.querySelector(`.quantity input`).value);
+  // console.log(`quantity is ${quantity}`);
+
+  let subTotal = product.querySelector(`.subtotal span`);
+  // console.log(`subtotal inner html is ${subTotal.innerHTML}`);
   let subTotalUpdated = price * quantity;
 
   return (subTotal.innerHTML = subTotalUpdated);
@@ -13,12 +17,16 @@ function updateSubtotal(product) {
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
   // end of test
 
-  // ITERATION 2
-  //... your code goes here
+  const products = document.querySelectorAll('.product');
+  console.log(products);
+
+  products.forEach((product) => {
+    updateSubtotal(product);
+  });
 
   // ITERATION 3
   //... your code goes here
